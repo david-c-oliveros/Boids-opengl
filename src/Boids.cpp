b2ec92de07bc925845e1d7b89ceca858ba5fab14
david <david3oliveros@gmail.com>
2022-12-11 17:45:58 -0500

    Set up OpenGL and GLFW boilerplate; got window rendering to screen

--- src/Boids.cpp
+++ src/Boids.cpp
@@ -1,6 +1,14 @@
 #include "Boids.h"
 
 
+unsigned int SCR_WIDTH = 1920;
+unsigned int SCR_HEIGHT = 1080;
+
+float fDeltaTime = 0.0f;
+float fLastFrame = 0.0f;
+float fCurrentFrame = 0.0f;
+
+
 Boids::Boids()
 {
     std::cout << "Boids has been created\n";
@@ -14,7 +22,38 @@ Boids::~Boids()
 
 bool Boids::Construct()
 {
-    std::cout << "Boids has been constructed\n";
+    /**********************************************************/
+    /**********************************************************/
+    /*               Initialize GLFW and GLEW                 */
+    /**********************************************************/
+    /**********************************************************/
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW\n";
+        return false;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Boids", NULL, NULL);
+
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return false;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "\nERROR: Failed to initialize GLEW\n";
+        return false;
+    }
 
     return true;
 }
@@ -22,5 +61,23 @@ bool Boids::Construct()
 
 void Boids::Start()
 {
-    std::cout << "Boids has been started\n";
+    while (!glfwWindowShouldClose(window))
+    {
+        fDeltaTime = fCurrentFrame - fLastFrame;
+        fLastFrame = fCurrentFrame;
+
+        Update(fDeltaTime);
+    }
+
+    glfwTerminate();
+}
+
+
+void Boids::Update(float fDeltaTime)
+{
+    glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
 }
--- src/Boids.h
+++ src/Boids.h
@@ -1,12 +1,24 @@
+#ifndef BOIDS_H
+#define BOIDS_H
+
 #include <iostream>
+#include <GL/glew.h>
+#include<GLFW/glfw3.h>
 
 
 class Boids {
+
+    private:
+        GLFWwindow *window;
+
     public:
         Boids();
         ~Boids();
 
         bool Construct();
         void Start();
+        void Update(float fDeltaTime);
 
 };
+
+#endif
